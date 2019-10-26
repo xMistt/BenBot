5865b49163ee514afb765ec625d1ca424b3c46a2
Oli <55298769+xMistt@users.noreply.github.com>
2019-10-26 21:07:52 +0100

    Update __init__.py

--- BenBot/__init__.py
+++ BenBot/__init__.py
@@ -1,101 +1,127 @@
-import requests
-name = 'BenBot'
+import aiohttp
+name = 'BenBot-async'
 
 BEN_BOT = 'http://benbotfn.tk:8080/api/'
 
 # Get any cosmetic, not checking cosmetic type.
 
-def getCosmetic(search):
+async def getCosmetic(search):
     url = BEN_BOT + 'cosmetics/search?displayName=' + search
-    benResponse = requests.get(url).json()
-    return benResponse
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            return benResponse
 
-def getCosmeticId(search):
+async def getCosmeticId(search):
     url = BEN_BOT + 'cosmetics/search?displayName=' + search
-    benResponse = requests.get(url).json()
-    return benResponse['id']
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            return benResponse['id']
 
-def getCosmeticFromId(search):
+async def getCosmeticFromId(search):
     url = BEN_BOT + 'cosmetics/search?id=' + search
-    benResponse = requests.get(url).json()
-    return benResponse
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            return benResponse
 
 # Gets cosmetic with type Outfit.
 
-def getSkin(search):
+async def getSkin(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Outfit':
-            return cosmetic
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Outfit':
+                    return cosmetic
 
-def getSkinId(search):
+async def getSkinId(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Outfit':
-            return cosmetic['id']
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Outfit':
+                    return cosmetic['id']
 
 # Gets cosmetic with type Harvesting Tool.
 
-def getPickaxe(search):
+async def getPickaxe(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Harvesting Tool':
-            return cosmetic
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Harvesting Tool':
+                    return cosmetic
 
-def getPickaxeId(search):
+async def getPickaxeId(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Harvesting Tool':
-            return cosmetic['id']
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Harvesting Tool':
+                    return cosmetic['id']
 
 # Gets cosmetic with type Back Bling.
 
-def getBackpack(search):
+async def getBackpack(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Back Bling':
-            return cosmetic
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Back Bling':
+                    return cosmetic
 
-def getBackpackId(search):
+async def getBackpackId(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Back Bling':
-            return cosmetic['id']
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Back Bling':
+                    return cosmetic['id']
 
 # Gets cosmetic with type Glider.
 
-def getGlider(search):
+async def getGlider(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Glider':
-            return cosmetic
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Glider':
+                    return cosmetic
 
-def getGliderId(search):
+async def getGliderId(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Glider':
-            return cosmetic['id']
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Glider':
+                    return cosmetic['id']
 
 # Gets cosmetic with type Emote.
 
-def getEmote(search):
+async def getEmote(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Emote':
-            return cosmetic
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Emote':
+                    return cosmetic
 
-def getEmoteId(search):
+async def getEmoteId(search):
     url = BEN_BOT + 'cosmetics/search/multiple?displayName=' + search
-    benResponse = requests.get(url).json()
-    for cosmetic in benResponse:
-        if cosmetic['type'] == 'Emote':
-            return cosmetic['id']
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            benResponse = await r.json()
+            for cosmetic in benResponse:
+                if cosmetic['type'] == 'Emote':
+                    return cosmetic['id']
